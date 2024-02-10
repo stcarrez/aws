ef8713cd068882460c842d60cc574083d46ce704
Stephane Carrez <Stephane.Carrez@gmail.com>
2024-02-10 16:57:16 +0100

    Avoid the unfortunate GNATcoll dependency
    - Fix compilation

--- config/ssl/openssl/wrappers.c
+++ config/ssl/openssl/wrappers.c
@@ -172,7 +172,7 @@ void __aws_EVP_MD_CTX_free(EVP_MD_CTX *ctx)
   EVP_MD_CTX_destroy(ctx);
 }
 
-bool __aws_gnatcoll_sync_bool_compare_and_swap_access
+int __aws_gnatcoll_sync_bool_compare_and_swap_access
     (void** ptr, void* oldval, void* newval)
 {
 
